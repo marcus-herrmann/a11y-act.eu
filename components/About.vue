bda6436949a5fe626727eb109de60753fdf2e0e3
Marcus Herrmann <505181+marcus-herrmann@users.noreply.github.com>
2023-10-20 06:53:29 +0200

    Update About.vue  with non directive2102.eu link

--- components/About.vue
+++ components/About.vue
@@ -5,7 +5,7 @@
 
       <p>This website is a private, non-official project - and by no means a complete neither a legal reflection of EU Directive 2019/882. I, a web developer living in Europe interested in web accessibility, tried to wrap my head around this directive.</p>
 
-      <p><a href="https://directive2102.eu">If you are looking for information for Directive 2016/2102, often known as "EU Web Accessibility Directive", please click here.</a></p>
+      <p><a href="https://digital-strategy.ec.europa.eu/en/policies/web-accessibility-directive-standards-and-harmonisation">If you are looking for information for Directive 2016/2102, often known as "EU Web Accessibility Directive", please click here.</a></p>
 
       <p>Since this project is <a href="https://github.com/marcus-herrmann/a11y-act.eu">open source and on GitHub</a> feel free to join and correct formulations if necessary.</p>
     </div>
